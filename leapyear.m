052c106efc712578db4e3b8fa28e14ae76d01757
Joshua Call <joshuacall@protonmail.com>
2024-02-18 18:29:36 -0700

    Implement leapyear function
    This commits creates an initial implementation for the leapyear function

---
+++ leapyear.m
@@ -0,0 +1,21 @@
+%% Copyright (C) 2024  Joshua Call
+%% 
+%% This library is free software; you can redistribute it and/or
+%% modify it under the terms of the GNU Lesser General Public
+%% License as published by the Free Software Foundation; either
+%% version 2.1 of the License, or (at your option) any later version.
+%% 
+%% This library is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%% Lesser General Public License for more details.
+%% 
+%% You should have received a copy of the GNU Lesser General Public
+%% License along with this library; if not, write to the Free Software
+%% Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+%% USA
+
+function is_leap_year = leapyear(year)
+  year = floor(year);
+  is_leap_year = (mod(year,4) == 0) & ((mod(year,100) ~= 0) | (mod(year,400) == 0));
+end
