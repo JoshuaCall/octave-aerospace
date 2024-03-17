a0217df428777196fb096df46cfe73ef6c18a0a8
Joshua Call <joshuacall@protonmail.com>
2024-03-17 04:49:43 -0600

    Make improvements to convtemp
    This commit makes some improvements to convtemp: avoids unnecessary
    calculations, add unit tests, and handles unrecognized inputs.

--- src/convtemp.m
+++ src/convtemp.m
@@ -11,6 +11,12 @@
 function convertedValues = convtemp(valuesToConvert,inputTemperatureUnits,outputTemperatureUnits)
 % convert temperature units
   
+  % if input and output units are the same, return without running the rest of the function
+  if strcmp(inputTemperatureUnits,outputTemperatureUnits)
+    convertedValues = valuesToConvert;
+    return;
+  end
+
   % first, convert the input to Celsius
   if strcmp(inputTemperatureUnits,'K')
     valuesToConvert = valuesToConvert - 273.15;
@@ -18,6 +24,9 @@ function convertedValues = convtemp(valuesToConvert,inputTemperatureUnits,output
     valuesToConvert = (valuesToConvert - 32)*5/9;
   elseif strcmp(inputTemperatureUnits,'R')
     valuesToConvert = (valuesToConvert - 491.67)*5/9;
+  % if inputTemperatureUnits is not valid, throw error
+  elseif ~strcmp(inputTemperatureUnits,'C')
+    error("argument to convtemp inputTemperatureUnits is not valid, see documentation")
   end
 
   % next, convert the Celsius value to the output units
@@ -27,7 +36,23 @@ function convertedValues = convtemp(valuesToConvert,inputTemperatureUnits,output
     valuesToConvert = valuesToConvert*9/5+32;
   elseif strcmp(outputTemperatureUnits,'R')
     valuesToConvert = (valuesToConvert+273.15)*9/5;
+  % if outputTemperatureUnits is not valid, throw error
+  elseif ~strcmp(outputTemperatureUnits,'C')
+    error("argument to convtemp outputTemperatureUnits is not valid, see documentation")
   end
 
   convertedValues = valuesToConvert;
 end
+
+%!assert (abs(convtemp([0 100 -40],'C','F') - [32 212 -40]) < 0.00001)
+%!error convtemp([0 0 0], 'X', 'C')
+%!error convtemp([0 0 0], 'C', 'X')
+%!error convtemp([0 0 0], 'L', 'X')
+%!assert (abs(convtemp([0 0 0],'C',"C") - [0 0 0]) < 0.00001)
+%!assert (abs(convtemp([0 0 0],'F',"F") - [0 0 0]) < 0.00001)
+%!assert (abs(convtemp(0,"K","C") - -273.15) < 0.00001)
+%!assert (abs(convtemp([0 32 1000],'F','C') - [-17.7778 0 537.778]) < 0.001)
+%!assert (abs(convtemp([0 212],'F',"F") - [0 212]) < 0.00001)
+%!assert (abs(convtemp([0 250 0],'R',"C") - [-273.15 -134.261 -273.15]) < 0.001)
+%!assert (abs(convtemp([0 0 0 0],'C',"R") - [491.67 491.67 491.67 491.67]) < 0.001)
+%!assert (abs(convtemp([0; 273.15; 373.15],"K",'C') - [-273.15; 0; 100]) < 0.00001)
